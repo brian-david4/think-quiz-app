627dea530ba5f3bedecec0dfc71ab6fa40a41f2a
Brian David <bdavid1530@gmail.com>
2025-02-07 20:09:17 +0000

    Create basic app

---
+++ think/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <main className={styles.main}>
+        <h1>think!</h1>
+      </main>
+    </>
+  );
+}
